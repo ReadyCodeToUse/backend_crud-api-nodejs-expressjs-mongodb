910e3fac2ef7aa7dce4f9429e55dc126d8e82193
Andreaioele <a.ioele@icloud.com>
2023-03-13 00:11:35 +0100

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 const express = require('express')
-const dotenv = require('dotenv')
-
-import connectDB from './utils/connectDB';
+const dotenv = require('dotenv').config();
 
 
-dotenv.config();
+
+const connectDB = require("../utils/connectDB.js");
 
 
-const app = express()
-
-
+const app = express();
 
 app.listen(process.env.SERVER_PORT, () =>{
    console.log(`Server is runnign on port: ${process.env.SERVER_PORT}`);
--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,6 +1,5 @@
-import mongoose from 'mongoose';
-
-const dbUrl = `mongodb://${process.env.MONGODB_DATABASE_NAME}:${process.env.MONGODB_PASSWORD}@localhost:6000/${process.env.MONGODB_DATABASE_NAME}?authSource=admin`;
+const {mongoose} =require('mongoose');
+const dbUrl = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@localhost:6000/${process.env.MONGODB_DATABASE_NAME}?authSource=admin`;
 
 const connectDB = async () => {
     try {
@@ -8,9 +7,10 @@ const connectDB = async () => {
         console.log('Database connected...');
         console.log(dbUrl);
     } catch (error) {
+        console.log(dbUrl);
         console.log(error.message);
         setTimeout(connectDB, 5000);
     }
 };
 
-export default connectDB;
+module.exports = connectDB;
