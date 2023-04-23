4368fb37c0d40d4d28a82b0e30740e28c7eadcdf
Andreaioele <a.ioele@icloud.com>
2023-04-23 10:31:02 +0200

    [system] - align

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,16 +1,16 @@
 const { mongoose } = require('mongoose');
 const { genericLogger } = require('./logger');
 
-const dbUrl = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@localhost:6000/${process.env.MONGODB_DATABASE_NAME}?authSource=admin`;
-
+// const dbUrl = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@localhost:6000/${process.env.MONGODB_DATABASE_NAME}?authSource=admin`;
+const dbUrlProd = 'mongodb+srv://aioele:Andromeda50ioele@cluster-ai.xepqosm.mongodb.net/crud-api-db-prod?retryWrites=true&w=majority';
 const connectDB = async () => {
   try {
-    await mongoose.connect(dbUrl);
+    await mongoose.connect(dbUrlProd);
     console.log('Database connected...');
-    console.log(dbUrl);
+    console.log(dbUrlProd);
   } catch (error) {
     genericLogger.error(error);
-    console.log(dbUrl);
+    console.log(dbUrlProd);
     console.log(error.message);
     setTimeout(connectDB, 5000);
   }
