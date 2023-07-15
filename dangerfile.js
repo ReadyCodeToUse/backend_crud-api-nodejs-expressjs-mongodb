18e430bb3f18a9be6a8230011470dc778b2d44d8
Andreaioele <a.ioele@icloud.com>
2023-07-15 23:43:06 +0200

    [EA-25] - edit regex

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,6 @@
 const { danger, fail } = require('danger');
 
-const pattern = prompt('^[A-Z]{2}-\\d{2} - .+$;');
-
-const regExpConst = new RegExp(`${pattern}`, 'gi');
+const regExpConst = /^[A-Z]{2}-\d{2} - .+$;/gi;
 const prTitle = danger.github.pr.title;
 if (!prTitle.match(regExpConst)) {
   fail('Danger is available only in test');
