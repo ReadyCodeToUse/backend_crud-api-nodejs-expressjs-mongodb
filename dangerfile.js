4f4970c8ce7bdecbb5132fd5763e8fe179d55f19
Andreaioele <a.ioele@icloud.com>
2023-07-16 00:07:43 +0200

    [EA-25] - fix regex

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,6 @@
 const { danger, fail } = require('danger');
 
-const regExpConst = /^[A-Z]{2}-\d{2} - .+$;/gi;
+const regExpConst = /^[A-Z]{2}-\d{2} - .+$/;
 const prTitle = danger.github.pr.title;
 if (!prTitle.match(regExpConst)) {
   fail(`${prTitle} is not allowed. Please follow the format EA-00 - Text`);
